b075cc86c31e51c0b7f833448f30eb792d0a7478
Zachary Nemeroff <zanemeroff@gmail.com>
2019-11-18 18:27:09 -0700

    Finish iteration 1

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,12 +2,33 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+
 
 describe('Turn', function() {
 
-  it('should be a function', function() {
-    const card = new Card();
-    expect(Card).to.be.a('function');
+  it('should accept a guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.returnGuess()).to.equal('pug');
+  });
+
+  it('return card object', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.returnCard()).to.equal(card);
+  });
+
+  it('should evaluate a guess', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.evaluateGuess()).to.equal(false);
+  });
+
+  it('should give feedback', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
+    expect(turn.giveFeedback()).to.equal('incorrect!');
   });
 
 });
